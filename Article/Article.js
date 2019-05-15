ce04d33de084c9aeeb9c2a0d63b6c6846dbd0632
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-15 16:29:21 -0400

    MVP met

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,20 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'Click to Expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', () => this.expandArticle())
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open')
+    this.expandButton.textContent = (this.expandButton.textContent === 'Click to Expand') ? 'Click to Close' : 'Click to Expand'
   }
 }
 
@@ -26,4 +28,12 @@ class Article {
 
 */
 
-let articles;
+const articles = document.querySelectorAll('.article');
+
+articles.forEach(article => {
+  const individualArticle = new Article(article)
+})
+
+console.log(articles)
+
+// let articles;
